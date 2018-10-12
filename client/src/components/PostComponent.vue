79f509e1b9c57ffe4fa5b88e259099f501ac099e
Ivan5 <converse_ivan@hotmail.com>
2018-10-12 13:09:10 -0500

    implements add and delete post function

--- client/src/components/PostComponent.vue
+++ client/src/components/PostComponent.vue
@@ -1,11 +1,27 @@
 <template>
-  
+  <div class="container">
+    <h1>Lastest Posts</h1>
+    <div class="create-post">
+      <label for="create-post">Say Something...</label>
+      <input type="text" id="create-post" v-model="text" placeholder="Create a post">
+      <button v-on:click="createPost">Post</button>
+    </div>
+    <hr>
+    <p class="error" v-if="error">{{error}}</p>
+    <div class="posts-container">
+      <div class="post" v-for="(post,index) in posts" v-bind:item="post" v-bind:index="index" v-bind:key="post._id" v-on:dblclick="deletePost(post._id)">
+
+        {{ `${post.createdAt.getDate()}/${post.createdAt.getMonth()}/${post.createdAt.getFullYear()}`}}
+        <p class="text">{{ post.text }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import PostService from '../PostService';
 export default {
-  name: 'Post Component',
+  name: 'PostComponent',
   data() {
     return {
       posts: [],
@@ -18,6 +34,16 @@ export default {
       this.posts = await PostService.getPosts();
     } catch (error) {
       this.error = error.message;
+    }
+  },
+  methods:{
+    async createPost(){
+      await PostService.insertPost(this.text);
+      this.posts = await PostService.getPosts();
+    },
+    async deletePost(id){
+      await PostService.deletePost(id);
+      this.posts = await PostService.getPosts();
     }
   }
 }
